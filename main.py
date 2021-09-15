2ebad85f5e821a9d0467e897447c6beaf56e5b57
dinads <90200168+dinads@users.noreply.github.com>
2021-09-15 15:14:12 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.py
@@ -0,0 +1,8 @@
+basic.show_arrow(ArrowNames.EAST)
+basic.clear_screen()
+basic.show_icon(IconNames.HEART)
+basic.clear_screen()
+basic.show_arrow(ArrowNames.EAST)
+basic.clear_screen()
+print("Love You")
+print
---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.showArrow(ArrowNames.East)
+basic.clearScreen()
+basic.showIcon(IconNames.Heart)
+basic.clearScreen()
+basic.showArrow(ArrowNames.East)
+basic.clearScreen()
+console.log("Love You")
+console.log
